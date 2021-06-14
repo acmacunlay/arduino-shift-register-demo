4f3fd7abf71b6653c64cefd33aef634a8fa88167
acmacunlay <acmacunlay.project@gmail.com>
2021-06-14 13:33:05 +0800

    Update arduino-shift-register-demo.ino

--- arduino-shift-register-demo.ino
+++ arduino-shift-register-demo.ino
@@ -1,4 +1,4 @@
-#include <EEPROM.h>
+#include <EEPROM.h> // library used to access the MCU's internal EEPROM
 
 const byte latch = 5; // latch (Pin 12) of the 595 connected to D5
 const byte clk = 6; // clock (Pin 11) of the 595 connected to D6
@@ -13,20 +13,21 @@ void setup() {
   pinMode(clk, OUTPUT);
   Serial.begin(baudrate);
   returnToPreviousState();
-  delay(2000);
+  delay(2000); // will pause for 2000 ms to show the previously stored value
 
 }
 
 void loop() {
 
-  delay(1000);
+  delay(1000); // pause for 1000 ms before starting the for loop
   for (int i = 0; i < 256; i++) {
     updateRegister(i);
-    delay(100);
+    delay(100); // pause for 100 ms before continuing the for loop
   }
 
 }
 
+// gets the value stored in the EEPROM at address 0
 void returnToPreviousState() {
   byte savedState = EEPROM.read(0);
   updateRegister(savedState);
@@ -34,10 +35,10 @@ void returnToPreviousState() {
 
 void updateRegister (int val) {
 
-  Serial.println(val);
+  Serial.println(val); // print the current value in the serial monitor (Ctrl + Shift + M)
   digitalWrite(latch, LOW); // unlocks the 595
   shiftOut(data, clk, MSBFIRST, val); // feeds the new value
   digitalWrite(latch, HIGH); // locks the 595
-  EEPROM.update(0, val);
+  EEPROM.update(0, val); // will store the new value in the EEPROM at address 0
 
 }
